5189a97d48d6746cf4816217539c38d8e28c7622
Vemahk <Vemahk20@gmail.com>
2018-06-28 23:06:46 -0500

    Pareto commit

---
+++ ParetoTrading/src/Pareto.java
@@ -0,0 +1,145 @@
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Arrays;
+import java.util.Random;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Pareto {
+	
+	public static boolean PAUSE = true;
+	public static int UPMS = 1;
+
+	public static void main(String[] args) throws InterruptedException {
+		final int SIZE = 1500;
+		final int START_MONEY = 10;
+		
+		int[] data = new int[SIZE];
+		Arrays.fill(data, START_MONEY);
+		
+		JFrame frame = createFrame(SIZE, data);
+		Thread render = startRenderThread(frame, 60);
+		
+		int uc = 0; // uc -> update count
+		Random rand = new Random();
+		while(true) {
+			if(UPMS != 0 && !PAUSE) {
+				synchronized(data) {
+					int a = rand.nextInt(SIZE);
+					//while(data[a] == 0) a = rand.nextInt(SIZE);
+					
+					int b = rand.nextInt(SIZE);
+					while(data[b] == 0) b = rand.nextInt(SIZE);
+					
+					data[a]++;
+					data[b]--;
+				}
+				
+				if(++uc >= UPMS) {
+					uc = 0;
+					Thread.sleep(1);
+				}
+			}else Thread.sleep(1);
+		}
+	}
+	
+	public static JFrame createFrame(int size, int[] data) {
+		JFrame frame = new JFrame("See Pareto?");
+		frame.setUndecorated(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JPanel display = new JPanel() {
+			@Override
+			public void paintComponent(Graphics g) {
+				super.paintComponent(g);
+				g.setColor(Color.WHITE);
+				g.drawString("UPMS: " + UPMS, 5, 15);
+				synchronized(data) {
+					Arrays.sort(data);
+					
+					//Top section
+					double max = data[size-1];
+					for(int i=0;i<size;i++)
+						g.drawLine(i, this.getHeight()/2-1, i, this.getHeight()/2 - 1 - (int)(data[i] / max * this.getHeight()/2));
+					
+					//Bottom section
+					int modeval = 0;
+					int modec = 0;
+					
+					int range = data[size-1] - data[0] + 1;
+					
+					int[] count = new int[range];
+					for(int i=0, c=0;i < size;i++) {
+						if(i!=0 && data[i] != data[i-1])
+							c++;
+						count[c]++;
+						if(modec == c) {
+							modeval++;
+						}else if(modec != c && count[c] > modeval) {
+							modec = c;
+							modeval = count[c];
+						}
+					}
+					
+					int heightrange = this.getHeight()/2 - 5;
+					for(int i=0;i<range;i++) {
+						
+						g.fillRect(i * this.getWidth() / range, this.getHeight()/2 + 5 + (int)((1-((double)count[i] / modeval)) * heightrange) , this.getWidth() / range, this.getHeight()/2);
+					}
+				}
+			}
+		};
+		display.setBackground(Color.BLACK);
+		display.setPreferredSize(new Dimension(size, 800));
+		
+		frame.setContentPane(display);
+		
+		frame.addKeyListener(new KeyListener() {
+			@Override public void keyPressed(KeyEvent e) {
+				if(e.getKeyCode() == KeyEvent.VK_ESCAPE) {
+					frame.dispose();
+					System.exit(0);
+				}
+				if(e.getKeyCode() == KeyEvent.VK_SPACE)
+					PAUSE = !PAUSE;
+				if(e.getKeyCode() == KeyEvent.VK_UP)
+					UPMS++;
+				if(e.getKeyCode() == KeyEvent.VK_DOWN && UPMS > 0)
+					UPMS--;
+			}	
+			
+			@Override public void keyReleased(KeyEvent e) {}
+			@Override public void keyTyped(KeyEvent e) {}
+		});
+		
+		frame.setResizable(false);
+		frame.pack();
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+		
+		return frame;
+	}
+	
+	public static Thread startRenderThread(JFrame frame, int FPS) {
+		Thread render = new Thread() {
+			public void run() {
+				while(true) {
+					if(!PAUSE)
+						frame.repaint();
+					try {
+						Thread.sleep(1000 / FPS);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		};
+		render.start();
+		return render;
+	}
+	
+}
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Mini-Projects
 Just a place where I make a few small programs in my off time.
+
+# Pareto
+This is a trading simulation that I heard about recently that I wanted to test out.
+With the default settings, 1500 'people' (represented as ints) start with 10 dollars, say. A random indexed person 'a' is chosen, and then receives a random indexed person's 'b' dollar. 'b' cannot be someone with 0 dollars, because you can't lose money you don't have (at least in this sim). What do you think would happen over a very long time?
+
+Well, you can try out this code to see! Controls for the frame are as such: Esc closes the program, Space pauses it, and the UP and DOWN arrow keys speed up and slow down the simulation, respectively.
